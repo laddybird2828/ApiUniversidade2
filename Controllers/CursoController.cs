7f98b64b6752f4bddcbcfb06e7afe5425ff868ee
Ladyla Mellina Santana <meydjane1@gmail.com>
2023-10-17 11:54:25 -0300

    4° commit

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -32,5 +32,16 @@ namespace apiUniversidade.Controllers
             return cursos;
 
         }
+
+        [HttpPost]
+        public ActionResult Post(Curso curso){
+            _context.Cursos.Add(curso);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetCurso", 
+            new{id = curso.Id},
+            curso);
+
+        }
     }
 }
